2cf157bf9c493b7d3b6fb9b6cb93c7e4e48df1bc
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-10-02 19:24:53 +0200

    Add files

---
+++ src/exercise-0x10/pangram.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+// Exercise 0x00 - include "pangram.h" here
+
+int main(int argc, char** argv) {
+
+  // Exercise 0x00 - Setup a test for your implementation
+  std::cout << "TEST - Exercise 0x00" << std::endl; 
+
+  // Exercise 0x01 - Setup a test for your implementation
+  std::cout << "TEST - Exercise 0x01" << std::endl;
+
+  return 0;
+}
+
+// Exercise 0x00 - Implement the function from "pangram.h"
+
+// Exercise 0x01 - Implement the function from "program.h"
---
+++ src/exercise-0x10/pangram.h
@@ -0,0 +1,7 @@
+#include <string>
+
+// Exercise 0x00
+bool pangram(std::string text);
+
+// Exercise 0x01
+bool pangram(std::string text, bool printable);
