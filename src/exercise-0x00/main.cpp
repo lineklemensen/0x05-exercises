bc9e77ae5b65b6012ed9ab1807ff963bc3a7a78c
Line Klemensen <202407497@post.au.dk>
2024-12-15 15:56:16 +0100

    Created main file and organized existing code

---
+++ src/exercise-0x00/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "twice.h"
+
+using namespace std;
+
+int main() {
+  
+  letter_seen_twice("Welcome to the wonderful world of software engineering");
+  letter_seen_twice("Uncopyrightable");
+
+  return 0;
+}
--- src/exercise-0x00/twice.cpp
+++ src/exercise-0x00/twice.cpp
@@ -1,13 +1,5 @@
 #include "twice.h"
 
-int main() {
-  
-  letter_seen_twice("Welcome to the wonderful world of software engineering");
-  letter_seen_twice("Uncopyrightable");
-
-  return 0;
-}
-
 // Exercise 0x00 && Exercise 0x01
 std::string letter_seen_twice(std::string text) {
   // Hint: You'll probably want a nested for-loop (one for the current character and one for the characters you've seen before)
