45b3680eb055394a4ee2d81835c57eadc32df2a3
Line Klemensen <202407497@post.au.dk>
2024-12-15 16:27:28 +0100

    Definition of the letter_seen_twice function for exercise 0x00

--- src/exercise-0x00/twice.cpp
+++ src/exercise-0x00/twice.cpp
@@ -1,9 +1,22 @@
 #include "twice.h"
+#include <string>
 
 using namespace std;
 
 // Exercise 0x00 && Exercise 0x01
 string letter_seen_twice(string text) {
-  // Hint: You'll probably want a nested for-loop (one for the current character and one for the characters you've seen before)
+  //Outer loop to iterate over each character in the string
+  for (size_t i = 0; i < text.length(); ++i) {
+    //Inner loop to check all previous characters
+    for (size_t j = 0; j < i; ++j) {
+      //Checks if there's a match, i.e. if a character has appeared twice
+      if (text[i] == text[j]) {
+        //Returns the first character to appear twice as a string
+        return string(1, text[i]); 
+      }
+    }
+
+  }
+  //If no character appear twice, return an empty string
   return "";
 }
