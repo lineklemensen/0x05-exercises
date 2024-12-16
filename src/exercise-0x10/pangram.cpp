53e8b2c38779ff5d6cd17e21ecc29e47d4f05019
Line Klemensen <202407497@post.au.dk>
2024-12-16 14:04:57 +0100

    Fixed headers in the source code

--- src/exercise-0x10/pangram.cpp
+++ src/exercise-0x10/pangram.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
-// Exercise 0x00 - include "pangram.h" here
+#include <unordered_set>
+#include <algorithm>
+#include <cctype>
 #include "pangram.h"
 
 using namespace std;
 
 // Exercise 0x00 - Implement the function from "pangram.h"
 bool pangram(string text) {
-  
+
 }
 
 // Exercise 0x01 - Implement the function from "program.h"
