fedbdfd8c9517dae5595a03f4032aceda6d9f978
Line Klemensen <202407497@post.au.dk>
2024-12-16 14:17:34 +0100

    Definition of the pangram function for exercise 0x1+0, including comments explaining each step.

--- src/exercise-0x10/pangram.cpp
+++ src/exercise-0x10/pangram.cpp
@@ -6,8 +6,29 @@
 using namespace std;
 
 // Exercise 0x00 - Implement the function from "pangram.h"
-bool pangram(string text) {
+bool pangram(const string& text) {
 
+  /*Variable containing a set of characters*/
+  unordered_set<char> letters;
+
+  /*Range-based for loop to iterate over 
+  each character in the string*/
+  for (char c : text) {
+
+    /*Conditional statement to check if 
+    the character is a letter*/
+    if (isalpha(c)) {
+
+      /*Converts the character to lowercase 
+      and inserts it into the set "letters"*/
+      letters.insert(tolower(c));
+    }
+  }
+
+  /*Checks if the set "letters" contains
+   exactly 26 unique letters. 
+   If not, then the string is not a pangram*/
+  return letters.size() == 26;
 }
 
 // Exercise 0x01 - Implement the function from "program.h"
