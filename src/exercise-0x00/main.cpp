6ee854d864951a128f640240edf73a8e20924b42
Line Klemensen <202407497@post.au.dk>
2024-12-16 12:06:12 +0100

    Runtime error in the conditions of the if statements are fixed, and the program is run and tested

--- src/exercise-0x00/main.cpp
+++ src/exercise-0x00/main.cpp
@@ -17,7 +17,7 @@ int main() {
   The string is the result of the letter_seen_twice function,
   meaning that the string contains the first character to apper twice,
   i.e. if the string is emptry, no character has appeared twice*/
-  if (!result1.first != '\0') {
+  if (result1.first != '\0') {
     cout << "The first letter seen twice in the string is: '" << result1.first << "', found at indices [" << result1.second[0] << ", " << result1.second[1] << "]" << endl;
   } else {
     cout << "The string does'nt contain any letter more than one..." << endl;
@@ -27,7 +27,7 @@ int main() {
   auto result2 = letter_seen_twice("Uncopyrightable");
 
   //Conditional statement to test the function with the first string
-  if (!result2.first != '\0') {
+  if (result2.first != '\0') {
     cout << "The first letter seen twice in the string is: '" << result2.first << "', found at indices [" << result2.second[0] << ", " << result2.second[1] << "]" << endl;
   } else {
     cout << "The string does'nt contain any letter more than one..." << endl;
