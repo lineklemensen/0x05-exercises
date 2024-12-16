9ce1f19e12eb4d7aea56e09e9f9a6c135f639d56
Line Klemensen <202407497@post.au.dk>
2024-12-16 13:35:13 +0100

    Included header file in the souce code, and implementation of using namespace std

--- src/exercise-0x10/pangram.cpp
+++ src/exercise-0x10/pangram.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 // Exercise 0x00 - include "pangram.h" here
+#include "pangram.h"
+
+using namespace std;
 
 int main(int argc, char** argv) {
 
 // Test strings
-std::string case_0 = "A quick brown fox jumps over the lazy dog.";
-std::string case_1 = "When zombies arrive quickly, fax judge Pat.";
-std::string case_2 = "The five boxing wizards jump quickly.";
-std::string case_3 = "Amazingly few discotheques provide jukeboxes.";
+string case_0 = "A quick brown fox jumps over the lazy dog.";
+string case_1 = "When zombies arrive quickly, fax judge Pat.";
+string case_2 = "The five boxing wizards jump quickly.";
+string case_3 = "Amazingly few discotheques provide jukeboxes.";
 
   // Exercise 0x00 - Setup a test for your implementation
-  std::cout << "TEST - Exercise 0x00" << std::endl; 
+  cout << "TEST - Exercise 0x00" << endl; 
 
   // Exercise 0x01 - Setup a test for your implementation
-  std::cout << "TEST - Exercise 0x01" << std::endl;
+  cout << "TEST - Exercise 0x01" << endl;
 
   return 0;
 }
