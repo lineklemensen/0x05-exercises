26426ab7493ca0f1330473ac972774d09c466856
Line Klemensen <202407497@post.au.dk>
2024-12-16 13:36:48 +0100

    Header guards

--- src/exercise-0x10/pangram.h
+++ src/exercise-0x10/pangram.h
@@ -1,3 +1,6 @@
+#ifndef PANGRAM_H
+#define PANGRAM_H
+
 #include <string>
 
 // Exercise 0x00
@@ -5,3 +8,5 @@ bool pangram(std::string text);
 
 // Exercise 0x01
 bool pangram(std::string text, bool printable);
+
+#endif //PANGRAM_H
