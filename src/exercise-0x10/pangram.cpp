d5db22ec4b5a182d6a41bf906817e8ca520abc59
Line Klemensen <202407497@post.au.dk>
2024-12-16 13:42:30 +0100

    Implemtation of the pangram function for exercise 0x10 in the source code

--- src/exercise-0x10/pangram.cpp
+++ src/exercise-0x10/pangram.cpp
@@ -4,23 +4,9 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-
-// Test strings
-string case_0 = "A quick brown fox jumps over the lazy dog.";
-string case_1 = "When zombies arrive quickly, fax judge Pat.";
-string case_2 = "The five boxing wizards jump quickly.";
-string case_3 = "Amazingly few discotheques provide jukeboxes.";
-
-  // Exercise 0x00 - Setup a test for your implementation
-  cout << "TEST - Exercise 0x00" << endl; 
-
-  // Exercise 0x01 - Setup a test for your implementation
-  cout << "TEST - Exercise 0x01" << endl;
-
-  return 0;
-}
-
 // Exercise 0x00 - Implement the function from "pangram.h"
+bool pangram(string text) {
+  
+}
 
 // Exercise 0x01 - Implement the function from "program.h"
