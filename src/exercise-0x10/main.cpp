e8e76e3a879b8dd18bd756c4ff6a26b24e8ea1cd
Line Klemensen <202407497@post.au.dk>
2024-12-16 20:10:35 +0100

    Fixed prototype for the pangram function and correction of gramatical errors

--- src/exercise-0x10/main.cpp
+++ src/exercise-0x10/main.cpp
@@ -14,11 +14,11 @@ string case_4 = "Hello world!"; //Not a pangram
 
   // Exercise 0x00 - Setup a test for your implementation
   cout << "TEST - Exercise 0x00" << endl;
-  cout << "Case 0" << (pangram(case_0) ? "Yes" : "No") << endl;
-  cout << "Case 1" << (pangram(case_1) ? "Yes" : "No") << endl;
-  cout << "Case 2" << (pangram(case_2) ? "Yes" : "No") << endl;
-  cout << "Case 3" << (pangram(case_3) ? "Yes" : "No") << endl;
-  cout << "Case 4" << (pangram(case_4) ? "Yes" : "No") << endl;
+  cout << "Case 0: " << (pangram(case_0) ? "Yes" : "No") << endl;
+  cout << "Case 1: " << (pangram(case_1) ? "Yes" : "No") << endl;
+  cout << "Case 2: " << (pangram(case_2) ? "Yes" : "No") << endl;
+  cout << "Case 3: " << (pangram(case_3) ? "Yes" : "No") << endl;
+  cout << "Case 4: " << (pangram(case_4) ? "Yes" : "No") << endl;
 
   // Exercise 0x01 - Setup a test for your implementation
   cout << "TEST - Exercise 0x01" << endl;
--- src/exercise-0x10/pangram.h
+++ src/exercise-0x10/pangram.h
@@ -4,7 +4,7 @@
 #include <string>
 
 // Exercise 0x00
-bool pangram(std::string text);
+bool pangram(const std::string& text);
 
 // Exercise 0x01
 bool pangram(std::string text, bool printable);
