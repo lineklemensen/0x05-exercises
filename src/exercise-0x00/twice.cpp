7ad759dcdf6b7ba8f9804e2ddf25e66f912404ac
Line Klemensen <202407497@post.au.dk>
2024-12-16 11:33:42 +0100

    For exercise 0x03 the letter_seen_twice function has been altere. The return type is changed, so now it returns the letter seen twice and a vector containing the first two spots for the letter. The return statements has also been changed, both when a character is found twice, and when no character appears twice in the string.

--- src/exercise-0x00/twice.cpp
+++ src/exercise-0x00/twice.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-pair<char, vector<size_t>> letter_seen_twice(string text) {
+pair<char, vector<size_t>> letter_seen_twice(const string& text) {
   //Outer loop to iterate over each character in the string
   for (size_t i = 0; i < text.length(); ++i) {
     //Inner loop to check all previous characters
@@ -14,11 +14,11 @@ pair<char, vector<size_t>> letter_seen_twice(string text) {
       //Checks if there's a match, i.e. if a character has appeared twice
       if (text[i] == text[j]) {
         //Returns the first character to appear twice as a string
-        return string(1, text[i]); 
+        return {text[i], {j, i}};
       }
     }
 
   }
   //If no character appear twice, return an empty string
-  return "";
+return {'\0', {}};
 }
